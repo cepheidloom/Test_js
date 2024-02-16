1222c156f0bd388f4d4807820ff9e30dd64f4c51
cepheidloom <manishpratapsingh@jklu.edu.in>
2024-02-16 19:48:00 +0530

    First time using next JS

--- fire/main.js
+++ fire/main.js
@@ -1,30 +1,31 @@
 const firebase = require("firebase/app");
-const { getDatabase } = require('firebase/database');
-require("firebase/analytics");
 require("firebase/database");
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfnIi2V2Q24NwKEuT2NX6DoaAHdi-dp0U",
-  authDomain: "pet-finder-a55bf.firebaseapp.com",
-  databaseURL: "https://pet-finder-a55bf-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pet-finder-a55bf",
-  storageBucket: "pet-finder-a55bf.appspot.com",
-  messagingSenderId: "123046708378",
-  appId: "1:123046708378:web:6b93ab4de0c46a466d8b23",
-  measurementId: "G-TQ6L018E3L"
-};
+    apiKey: "AIzaSyBfnIi2V2Q24NwKEuT2NX6DoaAHdi-dp0U",
+    authDomain: "pet-finder-a55bf.firebaseapp.com",
+    databaseURL: "https://pet-finder-a55bf-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pet-finder-a55bf",
+    storageBucket: "pet-finder-a55bf.appspot.com",
+    messagingSenderId: "123046708378",
+    appId: "1:123046708378:web:6b93ab4de0c46a466d8b23",
+    measurementId: "G-TQ6L018E3L"
+  };
 
-greet();
+firebase.initializeApp(firebaseConfig);
 
-const app1 = firebase.initializeApp(firebaseConfig);
-const db = getDatabase();
+const db = firebase.database();
 
-var founderName = document.querySelector("#founderName");
-var founderPhone = document.querySelector("#founderPhone");
-var petName = document.querySelector("#petName")
-var foundLocation = document.querySelector("#foundLocation");
-var foundDate = document.querySelector("#foundDate");
-var petDescription = document.querySelector("#petDescription");
-
-var submitBtn = document.querySelector('input[type="submit"]');
-
+function saveData(founderName, founderPhone, foundLocation, foundDate, petDescription) {
+  firebase.database().ref('found/' + founderName).set({
+    founderName: founderName,
+    founderPhone: founderPhone,
+    foundLocation: foundLocation,
+    foundDate: foundDate,
+    petDescription: petDescription
+  })
+  .then(() => {alert("Data saved successfully")} )
+  .catch((error) => {alert("Data could not be saved." + error)});
+}
+module.exports = saveData;
