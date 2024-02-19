f1a019774a7ca586a5d942f68222229eb402afd6
cepheidloom <manishpratapsingh@jklu.edu.in>
2024-02-19 14:53:25 +0530

    Added changes

---
+++ test.js
@@ -0,0 +1,11 @@
+const bhanu_ki_aukat = function (bhanu) {
+  return bhanu.kismat;
+}
+
+const bhanu = {
+    kismat: "Footi",
+    dimag: "Chota"
+}
+
+const bhanu_ki_kasoti = bhanu_ki_aukat(bhanu);
+console.log(bhanu_ki_kasoti); // Footi 
